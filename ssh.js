7e64575de69b3a893a3142756d6716fc4eaa5929
Alexey Nesterenko <alexei.ne@gmail.com>
2019-08-03 13:11:20 +0700

    es6

--- ssh.js
+++ ssh.js
@@ -4,12 +4,12 @@ const yargs        = require('yargs');
 const SSHClientApp = require('./lib/SSHClientApp');
 const Helper       = require('./lib/Helper');
 
-var app = new SSHClientApp();
+let app = new SSHClientApp();
 
-app.run(yargs.argv).catch((err) => {
+app.run(yargs.argv).catch(err => {
     if (err.errno == 'ECONNREFUSED') {
         Helper.errorToTerminal(`Can't connect to ${err.address}`);
     } else {
-     Helper.errorToTerminal(err.message);
+        Helper.errorToTerminal(err);
     }
 });
